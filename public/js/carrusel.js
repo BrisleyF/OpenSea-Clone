0c23ca7d991b8cdc4f4ad83e3187919613cc0fc3
BrisleyF <fajardobrisley@gmail.com>
2023-01-05 15:41:18 -0400

    Cambios

--- public/js/carrusel.js
+++ public/js/carrusel.js
@@ -56,3 +56,33 @@ window.addEventListener('load', function () {
         ]
     });
 });
+
+
+window.addEventListener('load', function () {
+    new Glider(document.querySelector('.carousel__lista_3'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.carousel__anterior_3',
+            next: '.carousel__siguiente_3' 
+        },
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 450,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                // screens greater than >= 1024px
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
+    });
+});
