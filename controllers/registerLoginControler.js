f3c5d90644ae9c9f332c33663a1ce62478f4f3ae
BrisleyF <fajardobrisley@gmail.com>
2023-01-10 21:07:30 -0400

    Logica de register, login, logout

--- controllers/registerLoginControler.js
+++ controllers/registerLoginControler.js
@@ -1,7 +1,18 @@
-
+const User = require('../model/User');
 
 exports.registrar = async (req, res) => {
-    res.send('Registrado');
+    const {nombre, email, clave} = req.body;
+	console.log('req.body', req.body);
+
+	const usuario = new User({nombre, email, clave, date: Date()});
+
+	usuario.save(err => {
+		if (err) {
+			res.status(500).send('ERROR AL REGISTRAR EL USUARIO');
+		} else {
+			res.redirect('/login');
+		}
+	});
 };
 
 exports.registro = async (req, res) => {
@@ -9,7 +20,32 @@ exports.registro = async (req, res) => {
 };
 
 exports.autenticar = async (req, res) => {
+    const {email, clave} = req.body;
 
+	let usuario = User.findOne({email}, (err, usuario) => {
+		if (err) {
+			res.status(500).send('ERROR AL AUTENTICAR EL USUARIO');
+		} else if (!usuario) {
+			res.status(500).send('EL USUARIO NO EXISTE');
+		} else {
+			User.findOne({clave, email}, (err, usuario) => {
+				if (err) {
+					res.status(500).send('ERROR AL AUTENTICAR');
+				} else if (!usuario) {
+					res.status(500).send('LA CLAVE NO COINCIDE');
+				} else {
+					req.login(usuario, function(err) {
+						if(err){
+							Error('Error al crear session');
+							console.log('Error al crear session');
+						}
+						return res.redirect('/');
+					})
+                    
+				}
+			});
+		}
+	});
 }
 
 exports.login = async (req, res) => {
@@ -17,5 +53,8 @@ exports.login = async (req, res) => {
 };
 
 exports.logout = async (req, res) => {
-    res.send('Session cerrada')
+    req.logout(function(err) {
+		if (err) { return next(err); }
+		res.redirect('/login')
+	});
 };
