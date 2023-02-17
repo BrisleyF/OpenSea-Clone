af214b209c5d3564f88215483f903209b0836add
BrisleyF <fajardobrisley@gmail.com>
2023-02-17 18:19:21 -0400

    Cambios en el modelo user

--- model/User.js
+++ model/User.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     clave: { type: String, required: true },
     date: { type: Date },
     bio: { type: String },
     imagePerfil: { type: String },
     imageBanner: { type: String },
     balance: { type: Number },
+    balanceDiferido: { type: Number },
     articulos: { type: Array}
 
 });
