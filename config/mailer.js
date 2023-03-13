33e852389bbd1b56fb7f7af5e1154659de745a9e
BrisleyF <fajardobrisley@gmail.com>
2023-03-13 13:03:13 -0400

    configuracion de nodemailer

--- config/mailer.js
+++ config/mailer.js
@@ -1,14 +1,16 @@
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: 'fajardobrisley@gmail.com', // generated ethereal user
-      pass: 'yidsuoiqzzasybyk' // generated ethereal password
+      user: process.env.email, // generated ethereal user
+      pass: process.env.adminApi // generated ethereal password
     },
 });
+
 
 transporter.verify().then(() => {
     console.log('Listo para enviar el email');
