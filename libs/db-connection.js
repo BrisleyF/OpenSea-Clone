f83762201f870a7f5e016b302fceec1e3c2793a4
Brisley Daniela Fajardo <fajardobrisley@gmail.com>
2023-03-19 23:09:39 -0400

    Coneccion a la base de datos

--- libs/db-connection.js
+++ libs/db-connection.js
@@ -7,7 +7,7 @@ module.exports = function Connection() {
     if(!db) {
         console.log('se inicio la base de datos');
         mongoose.set('strictQuery', true);
-        db = mongoose.connect(process.env.MONGODB_URI);
+        db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/openSea' );
     }
 
     return db; 
